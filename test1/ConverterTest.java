f9cd0f9332b7920ff72ff50484559768d53a7df7
prtjohanson <pjohanson@verticalscope.com>
2023-01-08 11:53:20 +0200

    Remove extra whitespace. added test directories

---
+++ test1/ConverterTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @org.junit.jupiter.api.Test
+    void getCelsius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCelsius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFahrenheit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFahrenheit() {
+    }
+}
