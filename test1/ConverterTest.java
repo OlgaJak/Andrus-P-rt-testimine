4007b2980c15350f87ab4fc53d7b99efcbf9e0e2
OlgaJak <jakimenko83@mail.ru>
2023-01-15 12:21:51 +0200

    added testing

--- test1/ConverterTest.java
+++ test1/ConverterTest.java
@@ -1,20 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConverterTest {
 
     @org.junit.jupiter.api.Test
     void getCelsius() {
+        assertEquals(0.0, 0.0);
     }
 
     @org.junit.jupiter.api.Test
     void setCelsius() {
+        assertEquals(-35, -31);
+
+        assertEquals(0, -17.77);
     }
 
     @org.junit.jupiter.api.Test
     void getFahrenheit() {
+        assertEquals(0.0, 0.0);
     }
 
     @org.junit.jupiter.api.Test
     void setFahrenheit() {
+        assertEquals(30, 86);
+
+        assertEquals(0, 32);
+
     }
+
+    @Test
+    void setfahrenheit() {
+        Converter setfahrenheit = new Converter();
+        setfahrenheit.setFahrenheit(0);
+       assertThat(5).isEqualTo(5);
+    }
+
 }
--- test1/MainTest.java
+++ test1/MainTest.java
@@ -3,5 +3,6 @@ import junit.framework.TestCase;
 public class MainTest extends TestCase {
 
     public void testMain() {
+
     }
 }
